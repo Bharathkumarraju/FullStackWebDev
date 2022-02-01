95905965cc3a3063b4d63eb3342661ce5a469c12
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-02-01 10:46:22 +0800

    :bulb: add variables with message :bulb:

--- simple_usecase/index.js
+++ simple_usecase/index.js
@@ -3,6 +3,9 @@ var readlineSync = require('readline-sync');
 var userName = readlineSync.question('whats your name? ');
 console.log('my name is '+ userName+ '!!!');
 
+var welcomeMessage = "Welcome " + userName + "!!!";
+console.log(welcomeMessage)
+
 var favFood =  readlineSync.question('My favourite food is ', {
     hideEchoBack: true
 });
