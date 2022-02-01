a3f320f18fc3ca2efffec83d7f681bbd5e1b3d46
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-02-01 15:55:45 +0800

    :tada: groceryList[gorceryList.length -1] :tada:

---
+++ Arrays/index.js
@@ -0,0 +1,19 @@
+/**
+ * prepare a list of grocery items to buy
+ * add 5 items
+ * print the first item on the list
+ * print the third item on the list
+ * print the last item on the list
+ */
+
+var groceryList = ["Milk", "eggs", "vada pav", "samosa", "flowers", "bread"]
+
+console.log(groceryList[0])
+console.log(groceryList[2])
+
+console.log(groceryList.length)
+
+var howLongIsThisArray = groceryList.length
+console.log(howLongIsThisArray)
+
+console.log(groceryList[groceryList.length -1] )
