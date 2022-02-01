2a52a4397ce19de3211a92ffd1f558d25f574fae
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-02-01 10:06:30 +0800

    :rocket: hideEchoBack :rocket:

--- simple_usecase/index.js
+++ simple_usecase/index.js
@@ -1,7 +1,9 @@
-var readlineSync = require('readline-sync')
+var readlineSync = require('readline-sync');
 
-var userName = readlineSync.question('whats your name? ')
-console.log('my name is '+ userName+ '!!!')
+var userName = readlineSync.question('whats your name? ');
+console.log('my name is '+ userName+ '!!!');
 
-
-
+var favFood =  readlineSync.question('My favourite food is ', {
+    hideEchoBack: true
+});
+console.log('oh, ' + userName + '\'s favourite food is ' + favFood)
