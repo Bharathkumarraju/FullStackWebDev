30966192cadc7f3263c084526e917a9723897d50
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-02-01 13:01:37 +0800

    :tada: arithmetic operations with  JS :tada:

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,27 @@
 function add(numberOne, numberTwo) {
+    console.log("numberOne: ", numberOne, "numberTwo: ", numberTwo)
     var sum = numberOne + numberTwo;
     return sum;
 }
 
-var result = add(9, 9);
+function sub(numberOne, numberTwo){
+    console.log('numberOne: ', numberOne, 'numberTwo: ', numberTwo)
+    var minus = numberOne - numberTwo
+    return minus
+}
+
+function mul(numberOne, numberTwo) {
+    console.log('numberOne: ', numberOne, 'numberTwo: ', numberTwo)
+    var product = numberOne * numberTwo
+    return product
+}
+
+var result = add(99, 33);
 console.log(" The sum is " + result)
+
+var res = sub(29, 2);
+console.log("The subtraction is " + res)
+
+var res = mul(33, 3);
+console.log("the product is " + res)
+
