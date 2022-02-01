fee9afa60a41ccd1504aa09a12555a0e31dd11bd
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-02-01 14:42:40 +0800

    :technologist: loopings patterns :technologist:

--- loopings/index.js
+++ loopings/index.js
@@ -1,3 +1,19 @@
-for(var i = 1; i<19; i = i + 1){
+for(var i = 1; i<10; i = i + 1){
     console.log('HANUMANS' + i)
 }
+
+for(var i=22; i<27; i+=1){
+    console.log(i)
+}
+
+console.log("\n")
+
+var string = "";
+
+for(var i = 1; i<6; i=i+1) {
+    for (j = 0; j<i; j=j+1){
+        string = string + "* "
+    }
+    string = string + "\n";
+}
+console.log(string)
