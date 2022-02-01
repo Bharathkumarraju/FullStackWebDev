b1a6cfb869f3db70317551b1cde27519bee8f024
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-02-02 01:40:41 +0800

    :beers: arrays in loop :beers:

--- Arrays/index.js
+++ Arrays/index.js
@@ -4,9 +4,13 @@
  * print the first item on the list
  * print the third item on the list
  * print the last item on the list
- */
+*/
 
-var groceryList = ["Milk", "eggs", "vada pav", "samosa", "flowers", "bread"]
+function log(data){
+    console.log(data)
+}
+
+var groceryList = ["Milk", "eggs", "vada pav", "samosa", "flowers", "bread", "medicines","pencil"]
 
 console.log(groceryList[0])
 console.log(groceryList[2])
@@ -17,3 +21,8 @@ var howLongIsThisArray = groceryList.length
 console.log(howLongIsThisArray)
 
 console.log(groceryList[groceryList.length -1] )
+
+
+for (var i=0; i<groceryList.length; i++) {
+   log(groceryList[i]);
+}
