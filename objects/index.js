a13d689418908d92daa461bb4d2044de957fb7b2
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-02-02 11:41:01 +0800

    :boom: Objects in javascript :boom:

---
+++ objects/index.js
@@ -0,0 +1,24 @@
+// object is a key-value pair
+
+var superman = {
+  name: "Superman",
+  power: "flight",
+  costumeColor: "blue",
+  strength: 10000,
+  stealth: 0,
+  intelligence: 100,
+}
+
+var batman = {
+    name: "batman",
+    power: "martial arts",
+    costumeColor: "black",
+    strength: 100,
+    stealth: 100,
+    intelligence: 1000,
+}
+
+console.log(superman.costumeColor)
+console.log(batman.costumeColor)
+
+console.log(batman.intelligence > superman.intelligence)
